e31fc48629dc7ba7f45bf7962103ab7cff4f6065
Muzammil <muzammilbinmajid42@gmail.com>
2026-05-12 22:31:47 +0530

    Add Google verification meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+         <meta name="google-site-verification" content="PS6-685dYcaCXLVx30dHnYk0D6opi7rv3HLhiByzs80" />
+       </head>
       <body className="min-h-full flex flex-col">
         {children}
       </body>
